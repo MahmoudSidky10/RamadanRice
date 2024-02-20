4376c606f751a60e4cecf6f84758d9483f6d7df3
mahmoud <mahmoud.sidky@recapet.com>
2024-02-20 21:28:30 +0200

    updates

--- app/Http/Controllers/Admin/User/UsersController.php
+++ app/Http/Controllers/Admin/User/UsersController.php
@@ -228,7 +228,7 @@ class UsersController extends Controller
 
         $csvExporter = new \Laracsv\Export();
         $csvExporter->build($orders, $fields);
-        $csvExporter->download(__("orders") . "_" .  time() . ".xlsx");
+        $csvExporter->download(__("orders") . "-" . Carbon::today()->format('y-m-d') . ".xls");
     }
 
 
